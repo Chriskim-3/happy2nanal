5383c0002bea8f53f985612615acd78e121a9461
Chriskim-3 <43245402+Chriskim-3@users.noreply.github.com>
2024-08-11 16:36:49 +0900

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,13 +3,14 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase
 import { getDatabase } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  databaseURL: "YOUR_DATABASE_URL",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBtyjw-rPmCLoXK7O471upPawvJmp6jaWA",
+  authDomain: "nanal-9e758.firebaseapp.com",
+  databaseURL: "https://nanal-9e758-default-rtdb.firebaseio.com",
+  projectId: "nanal-9e758",
+  storageBucket: "nanal-9e758.appspot.com",
+  messagingSenderId: "1073199749734",
+  appId: "1:1073199749734:web:372da8e47d7366e1242ba7",
+  measurementId: "G-L20RGTW4TC"
 };
 
 const app = initializeApp(firebaseConfig);
