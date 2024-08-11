4837a647f871f195cf94f417677659c1924f41e1
Chriskim-3 <43245402+Chriskim-3@users.noreply.github.com>
2024-08-11 16:20:17 +0900

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,15 +1,18 @@
-// Firebase configuration
+// firebaseConfig.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyBtyjw-rPmCLoXK7O471upPawvJmp6jaWA",
-    authDomain: "nanal-9e758.firebaseapp.com",
-    projectId: "nanal-9e758",
-    storageBucket: "nanal-9e758.appspot.com",
-    messagingSenderId: "1073199749734",
-    appId: "1:1073199749734:web:372da8e47d7366e1242ba7",
-    measurementId: "G-L20RGTW4TC"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  databaseURL: "YOUR_DATABASE_URL",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
 
-// Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
-const analytics = firebase.getAnalytics(app);
-const database = firebase.database();
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+export { database };
